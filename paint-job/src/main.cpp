d376854e2475329d73720504002b1c9d752dcdca
ssl-101 <inquiresiham@gmail.com>
2026-05-09 23:14:00 -0400

    Established function prototypes.

--- paint-job/src/main.cpp
+++ paint-job/src/main.cpp
@@ -1,4 +1,20 @@
+//This program estimates the total paint job cost.
 #include <iostream>
+#include <iomanip>
+#include <cmath>
+
 using namespace std;
 
+//function prototypes
+int getNumRooms();
+int gallonsRoom();
+double getpriceper();
+double getSqft();
+void displayEstimate ( double laborCost, double paintCost,
+                     int gallons, double laborHours, 
+                     double totalCost);
+
 int main ()
+{
+    
+}
